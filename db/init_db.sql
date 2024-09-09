9ce791eaae94bd574aeb23fdd806a874495f5a36
rnk00 <ryunamkyong@inha.edu>
2024-09-09 17:02:57 +0900

    [feat] db modify2

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Users (
     user_id INT NOT NULL AUTO_INCREMENT, 
     user_name VARCHAR(10) NOT NULL,
+    user_email VARCHAR(40) NOT NULL,
     PRIMARY KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 
--- dep.ADMIN definition
 CREATE TABLE ADMIN (
     id INT NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
@@ -22,48 +22,29 @@ CREATE TABLE ADMIN (
 
 CREATE TABLE Categories (
     id INT NOT NULL AUTO_INCREMENT,
-    user_email VARCHAR(20),
+    category_user_id INT,
     color VARCHAR(7),
     title VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_user_id) REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- CREATE TABLE Blocks (
---     block_id SERIAL PRIMARY KEY,
---     start_time TIMESTAMP NOT NULL,
---     end_time TIMESTAMP NOT NULL,
---     duration time.Duration NOT NULL,
---     color VARCHAR(7) REFERENCES Categories(categories_id) ON DELETE CASCADE ON UPDATE NO ACTION,
---     background_image_url TEXT,
---     block_memo TEXT,
---     block_pin BOOLEAN NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE Blocks (
+    block_id INT NOT NULL AUTO_INCREMENT,
+    category_id INT,
+    start_time INT NOT NULL,
+    end_time INT NOT NULL,
+    duration INT NOT NULL,
+    color VARCHAR(7),
+    background_image_url VARCHAR(50),
+    block_memo VARCHAR(100),
+    PRIMARY KEY(block_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(id) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-
--- -- CREATE TABLE CategoriesBlocks (
--- --     categories_id INT REFERENCES categories(categories_id),
--- --     block_id INT REFERENCES Blocks(block_id),
--- --     PRIMARY KEY(categories_id, block_id)
--- -- );
-
--- /* CREATE TABLE Tags (
---     tag_id SERIAL PRIMARY KEY,
---     tag_name VARCHAR(100) UNIQUE NOT NULL
--- );--? */
-
--- /* CREATE TABLE CategoriesTags (
---     categories_id INT REFERENCES categories(categories_id),
---     tag_id INT REFERENCES Tags(tag_id),
---     PRIMARY KEY(categories_id, tag_id)
--- ); */
-
--- CREATE TABLE Calendar (
---     date DATE PRIMARY KEY,
---     representative_block_id INT REFERENCES Blocks(block_id)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- -- CREATE TABLE CalendarBlocks (
--- --     date DATE REFERENCES Calendar(date),
--- --     block_id INT REFERENCES Blocks(block_id),
--- --     PRIMARY KEY(date, block_id)
--- -- );
+CREATE TABLE Calendar (
+    date_id INT NOT NULL AUTO_INCREMENT,
+    representative_block_id INT,
+    PRIMARY KEY(date_id),
+    FOREIGN KEY (representative_block_id) REFERENCES Blocks(block_id) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
