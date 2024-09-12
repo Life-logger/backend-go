9cddbbb727583656c588dd9b3060a3be194c5092
rnk00 <ryunamkyong@inha.edu>
2024-09-12 15:39:45 +0900

    add time

--- server/service/blocks/createBlockService.go
+++ server/service/blocks/createBlockService.go
@@ -3,7 +3,14 @@ package blocks
 import (
 	"lifelogger/server/domain/blocks/dto"
 	model "lifelogger/server/domain/blocks/model"
+	"time"
 )
+
+const DateTimeFormat = "2006-01-02 15:04:05" // 시간 포맷 상수
+
+var Now = func() time.Time {
+	return time.Now() // 현재 시간을 반환하는 함수
+}
 
 type (
 	CreateBlockService interface {
