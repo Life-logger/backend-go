d0a445dba482a5b5ddc50d02e760c155c29e7670
yeonsooso <janu10.sys@gmail.com>
2024-09-12 16:26:32 +0900

    [feat] send user data to db

--- server/controller/loginController.go
+++ server/controller/loginController.go
@@ -1,13 +1,22 @@
 package controller
 
 import (
+	"lifelogger/server/di"
+	"lifelogger/server/domain/users/dto"
+	"lifelogger/server/service/users"
 	"lifelogger/server/util"
 	"lifelogger/server/util/cookies"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-type LoginController struct{}
+type LoginController struct {
+	UserService users.CreateUserService
+}
+
+func NewLoginController(userService users.CreateUserService) *LoginController {
+	return &LoginController{UserService: userService}
+}
 
 func (a *LoginController) Login(c *fiber.Ctx) error {
 	code := c.Query("code")
@@ -17,6 +26,17 @@ func (a *LoginController) Login(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString("Faild to retrieve user info")
 	}
+	//
+	// Users 테이블에 사용자 정보 저장
+	createUserService, cleanup := di.InjectCreateUserService()
+	defer cleanup()
+
+	// CreateUser에 사용자 이메일과 닉네임을 전달
+	createUserService.CreateUser(dto.CreateUserReqDto{
+		UserEmail: email,
+		UserName:  nickname,
+	})
+	//
 	c.Cookie(cookies.MakeCookies("refreshToken", refreshToken)) // Refresh Token을 쿠키에 저장
 	c.Cookie(cookies.MakeCookies("accessToken", accessToken))   // Access Token을 쿠키에 저장
 	return c.SendString("hi, " + nickname + "!! \nYour email: " + email + "\nYour refreshToken: " + refreshToken)
