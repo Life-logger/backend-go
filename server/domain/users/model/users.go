bfb74887210e504617e84fc9d583030dda15c209
rokmcBum <138687371+rokmcBum@users.noreply.github.com>
2024-09-21 17:47:25 +0900

    feat: add toDto in user

--- server/domain/users/model/users.go
+++ server/domain/users/model/users.go
@@ -2,12 +2,14 @@ package users
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"lifelogger/server/domain/users/dto"
 )
 
 type User interface {
 	UserId() int
 	UserEmail() string
 	UserName() string
+	ToDto() dto.UserDto
 }
 
 type userImpl struct {
@@ -17,9 +19,8 @@ type userImpl struct {
 }
 
 // 새로운 사용자를 생성
-func NewUser(userId int, userEmail string, userName string) User {
+func NewUser(userEmail string, userName string) User {
 	i := new(userImpl)
-	i.userId = userId
 	i.userEmail = userEmail
 	i.userName = userName
 	return i
@@ -35,6 +36,14 @@ func (c userImpl) UserEmail() string {
 
 func (c userImpl) UserName() string {
 	return c.userName
+}
+
+func (c userImpl) ToDto() dto.UserDto {
+	return dto.UserDto{
+		UserEmail: c.userEmail,
+		UserName:  c.userName,
+	}
+
 }
 
 ////////////////////////////////////////////////////////////////
