74f843cee2d9a25d4bceb6c93cfe357753702565
rokmcBum <138687371+rokmcBum@users.noreply.github.com>
2024-09-21 17:46:22 +0900

    feat: add excludeUrl method to middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"fmt"
 	"lifelogger/server/util"
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,21 +12,33 @@ import (
 // AuthMiddleware는 사용자의 로그인 상태를 확인하는 미들웨어입니다.
 func AuthMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		accessToken := c.Cookies("accessToken")
-		log.Printf("Access Token: %s", accessToken)
-		if accessToken == "" {
-			// 쿠키에 액세스 토큰이 없으면 로그인 페이지로 리다이렉트
-			log.Println("No access token found")
-			panic("No access token found")
-		}
+		if !isExcludedUrl(c) {
+			accessToken := c.Cookies("accessToken")
+			log.Printf("Access Token: %s", accessToken)
+			if accessToken == "" {
+				// 쿠키에 액세스 토큰이 없으면 로그인 페이지로 리다이렉트
+				log.Println("No access token found")
+				panic("No access token found")
+			}
 
-		tokenGetter := util.NewTokenGetter()
-		_, _, err := tokenGetter.GetUserInfo(accessToken)
-		if err != nil {
-			// 액세스 토큰이 유효하지 않으면 로그인 페이지로 리다이렉트
-			panic("No access token found")
+			tokenGetter := util.NewTokenGetter()
+			_, _, err := tokenGetter.GetUserInfo(accessToken)
+			if err != nil {
+				// 액세스 토큰이 유효하지 않으면 로그인 페이지로 리다이렉트
+				panic("No access token found")
+			}
 		}
-
 		return c.Next()
 	}
+}
+
+func isExcludedUrl(c *fiber.Ctx) bool {
+	excludedUri := []string{"user"}
+	for _, uri := range excludedUri {
+		fmt.Println(string(c.Request().RequestURI()), uri)
+		if strings.Contains(string(c.Request().RequestURI()), uri) {
+			return true
+		}
+	}
+	return false
 }
